cf1241de61c0bf30ff1493e21437c58541178e28
Cameron Ross <elpenguino@gmail.com>
2016-03-21 16:43:56 -0300

    add verbose testing configuration

--- source/dyaml/tests/common.d
+++ source/dyaml/tests/common.d
@@ -37,6 +37,7 @@ void run(F ...)(string testName, void function(bool, F) testFunction,
     immutable string dataDir = "test/data";
     auto testFilenames = findTestFilenames(dataDir);
     bool verbose = false;
+    version(verboseTest) verbose = true;
 
     Result[] results;
     if(unittestExt.length > 0)
