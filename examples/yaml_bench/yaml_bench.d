96a63d79a2992810024437c6d25838390b7d775f
Cameron Ross <elpenguino@gmail.com>
2016-04-10 09:45:06 -0300

    fix yaml_bench for new interface

--- examples/yaml_bench/yaml_bench.d
+++ examples/yaml_bench/yaml_bench.d
@@ -1,35 +1,14 @@
 
-module dyaml.yaml_bench;
-//Benchmark that loads, and optionally extracts data from and/or emits a YAML file. 
+module yaml_bench;
+//Benchmark that loads, and optionally extracts data from and/or emits a YAML file.
 
 import std.conv;
 import std.datetime;
+import std.getopt;
+import std.outbuffer;
 import std.stdio;
 import std.string;
-import dyaml.all;
-
-///Print help information.
-void help()
-{
-    string help =
-        "D:YAML benchmark\n"
-        "Copyright (C) 2011-2014 Ferdinand Majerech\n"
-        "Usage: yaml_bench [OPTION ...] [YAML_FILE]\n"
-        "\n"
-        "Loads and optionally extracts data and/or dumps a YAML file.\n"
-        "\n"
-        "Available options:\n"
-        " -h --help          Show this help information.\n"
-        " -g --get           Extract data from the file (using Node.as()).\n"
-        " -d --dump          Dump the loaded data (to YAML_FILE.dump).\n"
-        " -r --runs=NUM      Repeat parsing the file NUM times.\n"
-        "    --reload        Reload the file from the diskl on every repeat\n"
-        "                    By default, the file is loaded to memory once\n"
-        "                    and repeatedly parsed from memory.\n"
-        " -s --scan-only    Do not execute the entire parsing process, only\n"
-        "                    scanning. Overrides '--dump'.\n";
-    writeln(help);
-}
+import wyaml;
 
 ///Get data out of every node.
 void extract(ref Node document)
@@ -66,49 +45,36 @@ void main(string[] args)
     bool get      = false;
     bool dump     = false;
     bool reload   = false;
-    bool scanOnly = false;
     uint runs = 1;
     string file = null;
 
-    //Parse command line args
-    foreach(arg; args[1 .. $]) 
-    {
-        auto parts = arg.split("=");
-        if(arg[0] == '-') switch(parts[0]) 
-        {
-            case "--help", "-h":      help(); return;
-            case "--get",  "-g":      get      = true; break;
-            case "--dump", "-d":      dump     = true; break;
-            case "--reload":          reload   = true; break;
-            case "--scan-only", "-s": scanOnly = true; break;
-            case "--runs", "-r":      runs     = parts[1].to!uint; break;
-            default: writeln("\nUnknown argument: ", arg, "\n\n"); help(); return;
-        }
-        else
-        {
-            if(file !is null)
-            {
-                writeln("\nUnknown argument or file specified twice: ", arg, "\n\n"); 
-                help(); 
-                return;
-            }
-
-            file = arg;
-        }
-    }
-    if(file is null)
-    {
-        writeln("\nFile not specified.\n\n"); 
-        help(); 
+    auto result = getopt(
+        args,
+        "get|g", "Extract data from the file (using Node.as()).", &get,
+        "dump|d", "Dump the loaded data (to YAML_FILE.dump).", &dump,
+        "reload", "Reload the file from the disk on every repeat. By default, the file is loaded to memory once and repeatedly parsed from memory.", &reload,
+        "runs|r", "Repeat parsing the file NUM times.", &runs,
+        );
+    if (!result.helpWanted && args.length == 1)
+        writeln("\nFile not specified.\n\n");
+    if (result.helpWanted || args.length == 1) {
+        defaultGetoptPrinter(
+        "D:YAML benchmark\n"
+        "Copyright (C) 2011-2014 Ferdinand Majerech\n"
+        "Copyright (C) 2016 Cameron \"Herringway\" Ross\n"
+        "Usage: yaml_bench [OPTION ...] [YAML_FILE]\n"
+        "\n"
+        "Loads and optionally extracts data and/or dumps a YAML file.\n", result.options);
         return;
     }
+    file = args[1];
 
     try
     {
         import std.file;
-        void[] fileInMemory;
-        if(!reload) { fileInMemory = std.file.read(file); }
-        void[] fileWorkingCopy = fileInMemory.dup;
+        string fileInMemory;
+        if(!reload) { fileInMemory = std.file.readText!string(file); }
+        string fileWorkingCopy = fileInMemory.dup;
 
         // Instead of constructing a resolver/constructor with each Loader,
         // construct them once to remove noise when profiling.
@@ -119,22 +85,17 @@ void main(string[] args)
         {
             // Loading the file rewrites the loaded buffer, so if we don't reload from
             // disk, we need to use a copy of the originally loaded file.
-            if(reload) { fileInMemory = std.file.read(file); }
-            else       { fileWorkingCopy[] = fileInMemory[]; }
-            void[] fileToLoad = reload ? fileInMemory : fileWorkingCopy;
-            if(scanOnly)
-            {
-                auto loader = Loader(fileToLoad);
-                loader.scanBench();
-                continue;
-            }
+            if(reload) { fileInMemory = std.file.readText!string(file); }
+            else       { fileWorkingCopy = fileInMemory; }
+            string fileToLoad = reload ? fileInMemory : fileWorkingCopy;
             auto loader        = Loader(fileToLoad);
             loader.resolver    = resolver;
             loader.constructor = constructor;
             auto nodes = loader.loadAll();
             if(dump)
             {
-                Dumper(file ~ ".dump").dump(nodes);
+                OutBuffer buf;
+                dumper(buf).dump(nodes);
             }
             if(get) foreach(ref node; nodes)
             {
